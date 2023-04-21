cf5041006737f788cdab5a4be9bba16c64184aaa
Vadym Bihun <bihunva@gmail.com>
2023-04-21 20:05:10 +0300

    Added Character list endpoint with filter

--- characters/views.py
+++ characters/views.py
@@ -1,9 +1,10 @@
 import random
 
+from django.db.models import QuerySet
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics, status
 
 from characters.models import Character
 from characters.serializers import CharacterSerializer
@@ -16,3 +17,16 @@ def get_random_character_view(request: Request) -> Response:
     random_character = Character.objects.get(pk=random_pk)
     serializer = CharacterSerializer(random_character)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CharacterListView(generics.ListAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self) -> QuerySet:
+        queryset = Character.objects.all()
+        name = self.request.query_params.get("name")
+
+        if name is not None:
+            return queryset.filter(name__icontains=name)
+
+        return queryset
